88476c3add58c6d30ab3de2bcc3308b7ea6cc6d1
Mathieu Enault <me94110@gmail.com>
2020-11-23 13:36:07 +0100

    Updates

---
+++ precache-manifest.536864cd69f97d58933deefabd3f1dc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698685aefbcc05afc00c257bcbbc10b9",
+    "url": "/My-To-Do-List/index.html"
+  },
+  {
+    "revision": "95777e95031bda37134d",
+    "url": "/My-To-Do-List/static/css/2.dec86d71.chunk.css"
+  },
+  {
+    "revision": "35a96eb106a90cff557f",
+    "url": "/My-To-Do-List/static/css/main.396fe24f.chunk.css"
+  },
+  {
+    "revision": "95777e95031bda37134d",
+    "url": "/My-To-Do-List/static/js/2.201f0783.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/My-To-Do-List/static/js/2.201f0783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35a96eb106a90cff557f",
+    "url": "/My-To-Do-List/static/js/main.3c4f715e.chunk.js"
+  },
+  {
+    "revision": "7f5e52a335cc9157bcd8",
+    "url": "/My-To-Do-List/static/js/runtime-main.0242097a.js"
+  },
+  {
+    "revision": "a26bc8ee8887491dd2f3f30f09399683",
+    "url": "/My-To-Do-List/static/media/fond-react.a26bc8ee.jpg"
+  }
+]);
